c328bd3233ff0997a9bc941902888e29f5e6ea79
Faisal <faisalgedi@uga.edu>
2016-09-08 22:59:37 -0400

    Person2 Update

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,25 +1,31 @@
 package edu.gatech.oad.antlab.person;
+
 import java.util.Random;
 
 /**
- *  A simple class for person 2
- *  returns their name and a
- *  modified string 
+ * A simple class for person 2
+ * returns their name and a
+ * modified string
  *
- * @author Bob
+ * @author Faisal Gedi
  * @version 1.1
  */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
-	 	/**
+	/**
+	 * Holds the persons real name
+	 */
+	private String name;
+
+	/**
 	 * The constructor, takes in the persons
 	 * name
+	 *
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String pname) {
+		name = pname;
+	}
+
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -31,41 +37,40 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
+		//Person 2 put your implementation here
 		Random random = new Random();
-		String jumble = scramble(random,input);
+		String jumble = scramble(random, input);
 
-
-
-	  return jumble;
+		return jumble;
 	}
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
+	 * @return the string representing the
+	 * object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 
 
-	public static String scramble( Random random, String inputString )
-	{
+	public static String scramble(Random random, String inputString) {
 		// Convert your string into a simple char array:
 		char a[] = inputString.toCharArray();
 
 		// Scramble the letters using the standard Fisher-Yates shuffle,
-		for( int i=0 ; i<a.length-1 ; i++ )
-		{
-			int j = random.nextInt(a.length-1);
+		for (int i = 0; i < a.length - 1; i++) {
+			int j = random.nextInt(a.length - 1);
 			// Swap letters
-			char temp = a[i]; a[i] = a[j];  a[j] = temp;
+			char temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
 		}
 
-		return new String( a );
+		return new String(a);
 	}
 
 }
